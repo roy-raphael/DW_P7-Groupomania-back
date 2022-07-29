761efe0253ebe3f322696c9dec88303cdbe0990e
Raphael Roy <roy.raphael.33@gmail.com>
2022-07-29 14:31:42 +0200

    feat: add files for Express server

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+export default app;
