5fb931dc585dc6e1ddf4f83a6c4abd31a13b3eeb
Raphael Roy <roy.raphael.33@gmail.com>
2022-08-01 17:23:31 +0200

    feat: add helmet to app.js

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 import express from 'express';
+import helmet from 'helmet';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
+});
+
+process.on('SIGTERM', () => {
+    server.close(() => {
+        console.log('Process terminated');
+    });
 });
 
 export default app;
