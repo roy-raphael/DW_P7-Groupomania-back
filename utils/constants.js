687fd2ad0e3b1f5c7ea7a7330e1592b9a385e475
Raphael Roy <roy.raphael.33@gmail.com>
2022-08-04 09:58:49 +0200

    feat: add constants utils file

---
+++ utils/constants.js
@@ -0,0 +1,8 @@
+export const POSTS_IMAGES_SAVE_PATH = 'images';
+
+export const MIME_TYPES = {
+    'image/jpg': 'jpg',
+    'image/jpeg': 'jpg',
+    'image/png': 'png',
+    'image/heic': 'heic'
+};
