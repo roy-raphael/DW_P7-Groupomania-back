538e88874f0986ecc86b29070d8b6d1edebd4b85
Raphael Roy <roy.raphael.33@gmail.com>
2022-08-04 09:53:44 +0200

    feat: update error utils to be more robust

--- utils/error-utils.js
+++ utils/error-utils.js
@@ -1,6 +1,6 @@
 export function formatErrorForResponse(error) {
     var errorJson = JSON.stringify({error});
-    if (errorJson === '{"error":{}}') {
+    if (errorJson === '{"error":{}}' || !errorJson.includes("message")) {
         errorJson = JSON.stringify({error : {
             name: error.name,
             message: error.message
