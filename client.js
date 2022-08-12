2723abb3964f9b95769d5f0d8ab26b6541221dd1
Raphael Roy <roy.raphael.33@gmail.com>
2022-08-12 20:59:58 +0200

    feat: add connection test at the start of the server

--- client.js
+++ client.js
@@ -2,4 +2,12 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
+// Connection test
+prisma.$connect()
+.then(prisma.$disconnect())
+.catch(error => {
+    console.error(error);
+    process.kill(process.pid, 'SIGTERM');
+});
+
 export default prisma
