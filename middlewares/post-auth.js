f188e16dca19c4e1882844301f3d1f18ef273a30
Raphael Roy <roy.raphael.33@gmail.com>
2022-10-26 11:15:00 +0200

    fix: authorize admins to edit or delete posts

--- middlewares/post-auth.js
+++ middlewares/post-auth.js
@@ -1,4 +1,5 @@
 import prisma from "../client.js"
+import { Role } from "@prisma/client";
 import {handleError} from '../utils/error-utils.js'
 
 // Check if the user has the right to modify the post
@@ -8,8 +9,20 @@ function postAuthorize(req, res, next) {
         if (!post) {
             return handleError(res, 404, new Error('No post found with this ID'));
         }
-        else if (post.authorId !== req.auth.userId) {
-            return handleError(res, 403, new Error('Unauthorized request'));
+        if (post.authorId !== req.auth.userId) {
+            prisma.user.findUnique({ where: { id: req.auth.userId }})
+            .then(user => {
+                if (!user) {
+                    return handleError(res, 401, new Error("No user exists for this token"));
+                }
+                if (user.role !== Role.ADMIN) {
+                    return handleError(res, 403, new Error('Unauthorized request'));
+                } else {
+                    req.post = post;
+                    next();
+                }
+            })
+            .catch(error => handleError(res, 500, error));
         }
         else {
             req.post = post;
